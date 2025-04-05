107a8ef8f571dc474946147769b192fb43f169e2
Benelini <eli.bencur@gmail.com>
2025-04-05 15:06:35 +0200

    chore: commands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: cloudflare({
-    imageService: "cloudflare",
-  }),
-  output: "server",
+  output: "static",
 });
